d40f1e3aa5e7f4cde02106678510d90cb3707133
ansaryergesh <yergeshovansar@gmail.com>
2019-08-24 02:00:20 +0600

    game interface coded

--- bin/main.rb
+++ bin/main.rb
@@ -11,18 +11,68 @@ puts "Once input, it will display in its appropriate box with letters either X o
 puts "Keep alternating moves until one of the players has drawn a row of three symbols or until no one can win."
 print "\n\n"
 
+players = players_info
+player1 = players[0]
+player2 = players[1]
 
+loop do
+    board = Board.new
+    grid  = board.grid
+    display(grid)
 
-# play again?
-play_again = 'y'
+loop do
+  begin
+    puts '1st Player: choose number between 1-9'
+    place = gets.chomp
+    result = board.move(player1.piece, place.to_i)
+    display(result[2]) 
+    rescue StandardError
+      puts "Error,You cannot use this place ! \n Try again :)"
+    retry
+  end
+  if result[0] == true
+    puts "#{player1.name} wins"
+    player1.amount += 1
+    puts "#{player1.name} amount is: #{player1.amount}"
+    puts "#{player2.name} amount is: #{player2.amount}"
+    break
+  end
+  if result[1] == 9
+    puts 'Draw'
+    puts "#{player1.name} amount is: #{player1.amount}"
+    puts "#{player2.name} amount is: #{player2.amount}"
+    break
+  end
+
+  begin
+    puts '2nd Player :Choose a place between 1 and 9'
+    place = gets.chomp
+    result = board.move(player2.piece, place.to_i)
+    display(result[2])
+    rescue StandardError
+      puts "Error,You cannot use this place ! \n Try again :)"
+    retry
+  end
+
+  next unless result[0] == true
+    puts "#{player2.name} win"
+    player2.amount += 1
+    puts "#{player1.name} amount is: #{player1.amount}"
+    puts "#{player2.name} amount is: #{player2.amount}"
+    break
+  end
+  play_again = 'y'
  
-while play_again == 'y'
+  while play_again == 'y'
 
-sleep 0.5
-puts "GAME OVER" 
+  sleep 0.5
+  puts "GAME OVER" 
 
 # play again?
-sleep 0.5
-puts "Play again? (y/n)"
-play_again = gets.chomp
+  sleep 0.5
+  puts "Play again? (y/n)"
+  play_again = gets.chomp
+  end
 end
+
+# play again?

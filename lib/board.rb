d32dacd4a2f3133f66eb43ec00adb98ac4d166e6
ansaryergesh <yergeshovansar@gmail.com>
2019-08-24 02:36:11 +0600

    fixed errors from rubocop suggestions

--- lib/board.rb
+++ lib/board.rb
@@ -32,12 +32,32 @@ class Win
   end
 
   def win_game
-    if @grid[0][0] == @grid[1][1] && @grid[1][1] == @grid[2][2] ||
-       @grid[2][0] == @grid[1][1] && @grid[1][1] == @grid[0][2] ||
-       @grid[0][0] == @grid[1][0] && @grid[1][0] == @grid[2][0] ||
+    diagonal || vertical || horizontal
+  end
+
+  # private methods
+  private
+
+  def diagonal
+    if @grid[0][0] == @grid[1][1] && @grid[1][1] == @grid[2][2] || @grid[2][0] == @grid[1][1] && @grid[1][1] == @grid[0][2]
+      return true
+    end
+
+    false
+  end
+
+  def vertical
+    if @grid[0][0] == @grid[1][0] && @grid[1][0] == @grid[2][0] ||
        @grid[0][1] == @grid[1][1] && @grid[1][1] == @grid[2][1] ||
-       @grid[0][2] == @grid[1][2] && @grid[1][2] == @grid[2][2] ||
-       @grid[0][0] == @grid[0][1] && @grid[0][1] == @grid[0][2] ||
+       @grid[0][2] == @grid[1][2] && @grid[1][2] == @grid[2][2]
+      return true
+    end
+
+    false
+  end
+
+  def horizontal
+    if @grid[0][0] == @grid[0][1] && @grid[0][1] == @grid[0][2] ||
        @grid[1][0] == @grid[1][1] && @grid[1][1] == @grid[1][2] ||
        @grid[2][0] == @grid[2][1] && @grid[2][1] == @grid[2][2]
       return true

4c0a802f366fdb225ee9c1f968dbaa25ec441d78
ansaryergesh <yergeshovansar@gmail.com>
2019-08-24 01:48:58 +0600

    started to write interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,4 @@
-require_relative "../lib/game.rb"
-require_relative "../lib/players.rb"
-require_relative "../lib/board.rb"
+
 puts "Welcome to tic-tac-toe"
 puts "Tic-tac-toe is a fun game that you can play any time and anywhere. But we create that game for you in Ruby. So, you can play it by using terminal"
 puts "Instructions: We Have a 3*3 box"
@@ -8,6 +6,8 @@ puts "A player has to input a number between 1 to 9"
 puts "Once input, it will display in its appropriate box with letters either X or O"
 puts "Keep alternating moves until one of the players has drawn a row of three symbols or until no one can win."
 print "\n\n"
+
+
 
 # play again?
 play_again = 'y'

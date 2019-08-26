777959c5824aec366f6fce0e028bb8ec56fe9674
ansaryergesh <yergeshovansar@gmail.com>
2019-08-27 00:31:34 +0600

    fixed some errors from rubucop suggestion

--- lib/game.rb
+++ lib/game.rb
@@ -13,30 +13,79 @@ class Game
 
   # private methods
   private
-
+  # Diagonals
   def diagonal
-    if @grid[0][0] == @grid[1][1] && @grid[1][1] == @grid[2][2] ||
-       @grid[2][0] == @grid[1][1] && @grid[1][1] == @grid[0][2]
+    left_diagonal || right_diagonal
+  end
+  
+  def left_diagonal
+    if @grid[0][0] == @grid[1][1] && @grid[1][1] == @grid[2][2]
+      return true
+    end
+
+    false
+  end
+  
+  def right_diagonal
+    if @grid[2][0] == @grid[1][1] && @grid[1][1] == @grid[0][2]
       return true
     end
 
     false
   end
 
+  # Verticals
   def vertical
-    if @grid[0][0] == @grid[1][0] && @grid[1][0] == @grid[2][0] ||
-       @grid[0][1] == @grid[1][1] && @grid[1][1] == @grid[2][1] ||
-       @grid[0][2] == @grid[1][2] && @grid[1][2] == @grid[2][2]
+    vertical_one || vertical_two || vertical_three
+  end
+
+  def vertical_one
+    if @grid[0][0] == @grid[1][0] && @grid[1][0] == @grid[2][0]
+      return true
+    end
+    
+    false
+  end
+
+  def vertical_two
+    if @grid[0][1] == @grid[1][1] && @grid[1][1] == @grid[2][1]
       return true
     end
 
     false
   end
 
+  def vertical_three
+    if @grid[0][2] == @grid[1][2] && @grid[1][2] == @grid[2][2]
+      return true
+    end
+
+    false
+  end
+
+  # Horizontals
   def horizontal
-    if @grid[0][0] == @grid[0][1] && @grid[0][1] == @grid[0][2] ||
-       @grid[1][0] == @grid[1][1] && @grid[1][1] == @grid[1][2] ||
-       @grid[2][0] == @grid[2][1] && @grid[2][1] == @grid[2][2]
+    horizontal_one || horizontal_two || horizontal_three
+  end
+
+  def horizontal_one
+    if @grid[0][0] == @grid[0][1] && @grid[0][1] == @grid[0][2] 
+      return true
+    end
+
+    false
+  end
+
+  def horizontal_two
+    if @grid[1][0] == @grid[1][1] && @grid[1][1] == @grid[1][2]
+      return true
+    end
+
+    false
+  end
+
+  def horizontal_three
+    if @grid[2][0] == @grid[2][1] && @grid[2][1] == @grid[2][2]
       return true
     end
 
